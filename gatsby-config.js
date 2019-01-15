1226279f046e57d5a7f5657f0bc5a057bf34a675
Ryan Choi <ryan.mingyu.choi@gmail.com>
2019-01-14 21:38:56 -0800

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    title: 'New Life New Vision Church',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        // The base url to your WP site.
+        baseUrl: 'wpbackend.nlnvchurch.org',
+        // WP.com sites set to true, WP.org set to false
+        hostingWPCOM: false,
+        // The protocol. This can be http or https.
+        protocol: 'http',
+        // Use 'Advanced Custom Fields' Wordpress plugin
+        useACF: true,
+        auth: {},
+        // Set to true to debug endpoints on 'gatsby build'
+        verboseOutput: false,
+        includedRoutes: [
+          "**/*/*/posts",
+          "**/*/*/pages",
+          "**/*/*/media",
+          "**/*/*/categories",
+          "**/*/*/tags",
+          "**/*/*/taxonomies",
+          "**/*/*/users",
+        ],
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-purgecss',
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
+  ],
+}
