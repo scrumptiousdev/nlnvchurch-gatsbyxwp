9bff20f464072c737ea8744078b229d9fbf7b621
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-03-18 23:48:29 -0700

    Finish mobile nav and about page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,75 +1,95 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { StaticQuery, graphql } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookSquare, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faBars, faLeaf, faQuoteRight, faBullhorn, faCameraRetro, faHeart, faMapSigns } from '@fortawesome/free-solid-svg-icons'
 
-const Navbar = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allWordpressPage(
-          filter: {
-            title: {
-              eq: "Contact and Social Setting"
-            }
-          }
-        ) {
-          edges {
-            node {
-              acf {
-                nlnv_social_youtube
-                nlnv_social_facebook
+class Navbar extends Component {
+  state = {
+    menuOpen: false
+  }
+
+  handleMenuToggle = (e) => {
+    e.preventDefault();
+    const { menuOpen } = this.state;
+    this.setState({ menuOpen: !menuOpen });
+  }
+
+  componentWillUnmount() {
+    this.setState({ menuOpen: false });
+  }
+
+  render() {
+    const { menuOpen } = this.state;
+
+    return (
+      <StaticQuery
+        query={graphql`
+          query {
+            allWordpressPage(
+              filter: {
+                title: {
+                  eq: "Contact and Social Setting"
+                }
+              }
+            ) {
+              edges {
+                node {
+                  acf {
+                    nlnv_social_youtube
+                    nlnv_social_facebook
+                  }
+                }
               }
             }
           }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <div className="nav__overlay" />
-        <nav className="nav">
-          <a className="js-transition nav__link nav__link-mobile" href="#">
-            <FontAwesomeIcon className="" icon={faBars} />
-          </a>
-          <a className="js-transition nav__link nav__link-logo" href="/">
-            <p className="nav__text">NL<br />NV</p>
-          </a>
-          <a className="js-transition nav__link" href="/about">
-            <FontAwesomeIcon className="nav__icon" icon={faLeaf} />
-            <p className="nav__text kor-main">교회소개</p>
-          </a>
-          <a className="js-transition nav__link" href="/series">
-            <FontAwesomeIcon className="nav__icon" icon={faQuoteRight} />
-            <p className="nav__text kor-main">설교영상</p>
-          </a>
-          <a className="js-transition nav__link" href="/news">
-            <FontAwesomeIcon className="nav__icon" icon={faBullhorn} />
-            <p className="nav__text kor-main">교회소식</p>
-          </a>
-          <a className="js-transition nav__link" href="/album">
-            <FontAwesomeIcon className="nav__icon" icon={faCameraRetro} />
-            <p className="nav__text kor-main">사진첩</p>
-          </a>
-          <a className="js-transition nav__link" href="/support">
-            <FontAwesomeIcon className="nav__icon" icon={faHeart} />
-            <p className="nav__text kor-main">협력/후원</p>
-          </a>
-          <a className="js-transition nav__link" href="/directions">
-            <FontAwesomeIcon className="nav__icon" icon={faMapSigns} />
-            <p className="nav__text kor-main">찾아오시는길</p>
-          </a>
-          <a className="nav__social" title="Facebook" href={data.allWordpressPage.edges[0].node.acf.nlnv_social_facebook} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon className="nav__icon" icon={faFacebookSquare} />
-          </a>
-          <a className="nav__social" title="YouTube" href={data.allWordpressPage.edges[0].node.acf.nlnv_social_youtube} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon className="nav__icon" icon={faYoutube} />
-          </a>
-        </nav>
-      </>
-    )}
-  />
-)
+        `}
+        render={data => (
+          <>
+            <div className={`nav__overlay${menuOpen ? ' opened' : ''}`} />
+            <nav className={`nav${menuOpen ? ' opened' : ''}`}>
+              <a className="js-transition nav__link nav__link-mobile" href="#" onClick={(e) => this.handleMenuToggle(e)}>
+                <FontAwesomeIcon className="" icon={faBars} />
+              </a>
+              <a className="js-transition nav__link nav__link-logo" href="/">
+                <p className="nav__text">NL<br />NV</p>
+              </a>
+              <a className="js-transition nav__link" href="/about">
+                <FontAwesomeIcon className="nav__icon" icon={faLeaf} />
+                <p className="nav__text kor-main">교회소개</p>
+              </a>
+              <a className="js-transition nav__link" href="/series">
+                <FontAwesomeIcon className="nav__icon" icon={faQuoteRight} />
+                <p className="nav__text kor-main">설교영상</p>
+              </a>
+              <a className="js-transition nav__link" href="/news">
+                <FontAwesomeIcon className="nav__icon" icon={faBullhorn} />
+                <p className="nav__text kor-main">교회소식</p>
+              </a>
+              <a className="js-transition nav__link" href="/album">
+                <FontAwesomeIcon className="nav__icon" icon={faCameraRetro} />
+                <p className="nav__text kor-main">사진첩</p>
+              </a>
+              <a className="js-transition nav__link" href="/support">
+                <FontAwesomeIcon className="nav__icon" icon={faHeart} />
+                <p className="nav__text kor-main">협력/후원</p>
+              </a>
+              <a className="js-transition nav__link" href="/directions">
+                <FontAwesomeIcon className="nav__icon" icon={faMapSigns} />
+                <p className="nav__text kor-main">찾아오시는길</p>
+              </a>
+              <a className="nav__social" title="Facebook" href={data.allWordpressPage.edges[0].node.acf.nlnv_social_facebook} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon className="nav__icon" icon={faFacebookSquare} />
+              </a>
+              <a className="nav__social" title="YouTube" href={data.allWordpressPage.edges[0].node.acf.nlnv_social_youtube} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon className="nav__icon" icon={faYoutube} />
+              </a>
+            </nav>
+          </>
+        )}
+      />
+    );
+  }
+}
 
 export default Navbar
--- src/templates/page-about.js
+++ src/templates/page-about.js
@@ -1,34 +1,173 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandshake, faChild, faGlobe, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
-export const AboutPage = ({ title, content, acf }) => {
-  return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {acf.nlnv_page_title}
-              </h2>
-			        <img src={acf.nlnv_hero_image.source_url} />
-              <div
-                className="content"
-                dangerouslySetInnerHTML={{ __html: content }}
-              />
+class AboutPage extends Component {
+  state = {
+    activeTab: 1,
+    staffActiveCount: 0
+  }
+
+  componentDidMount() {
+    const { acf: { nlnv_about_tabs } } = this.props;
+
+    let staffActiveNumber = 0;
+
+    for(let x = 1; x < 5; x++) {
+      const activeStatus = nlnv_about_tabs[`nlnv_about_staff_${x}`]['nlnv_staff_active'];
+      if (activeStatus) staffActiveNumber++;
+    }
+
+    this.setState({ staffActiveCount: staffActiveNumber });
+  }
+
+  handleTabClick = (tab) => {
+    document.querySelector('html, body').animate({
+      scrollTop: document.querySelector('.about__tab-contents').top
+    }, 500);
+    this.setState({ activeTab: tab });
+  }
+
+  render() {
+    const { activeTab, staffActiveCount } = this.state;
+    const { acf: { nlnv_page_title, nlnv_hero_image, nlnv_about_tabs, nlnv_about_service_grid } } = this.props;
+
+    return (
+      <>
+        <div className="about__banner text-center" style={{ backgroundImage: `url(${nlnv_hero_image.source_url})` }}>
+          <h1 className="nlnv__heading kor-main">{nlnv_page_title}</h1>
+          <hr className="divider divider--blue" />
+        </div>
+        <div className="about__tab-section">
+          <ul className="about__tab-lists">
+            <li className={`about__tab-list kor-main ${activeTab === 1 ? 'active' : ''}`} onClick={() => { this.handleTabClick(1) }}>{nlnv_about_tabs.nlnv_about_tab_1_title}</li>
+            <li className={`about__tab-list kor-main ${activeTab === 2 ? 'active' : ''}`} onClick={() => { this.handleTabClick(2) }}>{nlnv_about_tabs.nlnv_about_tab_2_title}</li>
+            <li className={`about__tab-list kor-main ${activeTab === 3 ? 'active' : ''}`} onClick={() => { this.handleTabClick(3) }}>{nlnv_about_tabs.nlnv_about_tab_3_title}</li>
+          </ul>
+          <ul className="about__tab-contents">
+            <li className={`about__tab-content about__tab-content--vision ${activeTab === 1 ? 'active' : ''}`}>
+              <div className="about__tab-content-wrapper kor-main">
+                <div className="row">
+                  <div className="col-md-6 offset-md-3">
+                    <p className="nlnv__subheading divider--wrap text--gold">{nlnv_about_tabs.nlnv_about_main_vision}</p>
+                    <FontAwesomeIcon className="about__vision-icon fa" icon={faHandshake} />
+                    <div dangerouslySetInnerHTML={{ __html: nlnv_about_tabs.nlnv_about_vision_1 }} />
+                    <hr className="divider" />
+                    <FontAwesomeIcon className="about__vision-icon fa" icon={faChild} />
+                    <div dangerouslySetInnerHTML={{ __html: nlnv_about_tabs.nlnv_about_vision_2 }} />
+                    <hr className="divider" />
+                    <FontAwesomeIcon className="about__vision-icon fa" icon={faGlobe} />
+                    <div dangerouslySetInnerHTML={{ __html: nlnv_about_tabs.nlnv_about_vision_3 }} />
+                    <hr className="divider" />
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li className={`about__tab-content about__tab-content--pastor ${activeTab === 2 ? 'active' : ''}`}>
+              <div className="about__tab-content-wrapper kor-main">
+                <div className="row">
+                  <div className="col-md-6 offset-md-3">
+                    <img className="about__tab-image margin-bottom-xl" src={nlnv_about_tabs.nlnv_about_introduction_photo.source_url} alt="" />
+                  </div>
+                  <div className="col-md-6 offset-md-3">
+                    <p className="nlnv__subheading divider--wrap text--gold">{nlnv_about_tabs.nlnv_about_introduction_title}</p>
+                    <div className="about__tab-introduction" dangerouslySetInnerHTML={{ __html: nlnv_about_tabs.nlnv_about_introduction }} />
+                    <p className="nlnv__subheading divider--wrap text--gold margin-top-xl">{nlnv_about_tabs.nlnv_about_resume_title}</p>
+                    <div dangerouslySetInnerHTML={{ __html: nlnv_about_tabs.nlnv_about_resume }} />
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li className={`about__tab-content about__tab-content--leader ${activeTab === 3 ? 'active' : ''}`}>
+              <div className="about__tab-content-wrapper kor-main">
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className={`col-md-${12 / staffActiveCount}`}>
+                      <img className="about__tab-image" src={nlnv_about_tabs.nlnv_about_staff_1.nlnv_about_staff_photo.source_url} alt="" />
+                      <p className="nlnv__subheading divider--wrap text--gold margin-top-sm">{nlnv_about_tabs.nlnv_about_staff_1.nlnv_about_staff_title}</p>
+                      <div className="about__tab-introduction margin-bottom-lg-max-desktop">
+                        <p>{nlnv_about_tabs.nlnv_about_staff_1.nlnv_about_staff_name}</p>
+                        <a className="about__tab-icon text-link margin-right-sm" href={`mailto:${nlnv_about_tabs.nlnv_about_staff_1.nlnv_about_staff_email}`}><FontAwesomeIcon icon={faEnvelope} /></a>
+                        {nlnv_about_tabs.nlnv_about_staff_1.nlnv_about_staff_phone !== '' && <a className="about__tab-icon text-link" href={`tel:${nlnv_about_tabs.nlnv_about_staff_1.nlnv_about_staff_phone}`}><FontAwesomeIcon icon={faPhone} /></a>}
+                      </div>
+                    </div>
+                    <div className={`col-md-${12 / staffActiveCount}`}>
+                      <img className="about__tab-image" src={nlnv_about_tabs.nlnv_about_staff_2.nlnv_about_staff_photo.source_url} alt="" />
+                      <p className="nlnv__subheading divider--wrap text--gold margin-top-sm">{nlnv_about_tabs.nlnv_about_staff_2.nlnv_about_staff_title}</p>
+                      <div className="about__tab-introduction margin-bottom-lg-max-desktop">
+                        <p>{nlnv_about_tabs.nlnv_about_staff_2.nlnv_about_staff_name}</p>
+                        <a className="about__tab-icon text-link" href={`mailto:${nlnv_about_tabs.nlnv_about_staff_2.nlnv_about_staff_email}`}><FontAwesomeIcon icon={faEnvelope} /></a>
+                        {nlnv_about_tabs.nlnv_about_staff_2.nlnv_about_staff_phone !== '' && <a className="about__tab-icon text-link" href={`tel:${nlnv_about_tabs.nlnv_about_staff_2.nlnv_about_staff_phone}`}><FontAwesomeIcon icon={faPhone} /></a>}
+                      </div>
+                    </div>
+                    <div className={`col-md-${12 / staffActiveCount}`}>
+                      <img className="about__tab-image" src={nlnv_about_tabs.nlnv_about_staff_3.nlnv_about_staff_photo.source_url} alt="" />
+                      <p className="nlnv__subheading divider--wrap text--gold margin-top-sm">{nlnv_about_tabs.nlnv_about_staff_3.nlnv_about_staff_title}</p>
+                      <div className="about__tab-introduction margin-bottom-lg-max-desktop">
+                        <p>{nlnv_about_tabs.nlnv_about_staff_3.nlnv_about_staff_name}</p>
+                        <a className="about__tab-icon text-link" href={`mailto:${nlnv_about_tabs.nlnv_about_staff_3.nlnv_about_staff_email}`}><FontAwesomeIcon icon={faEnvelope} /></a>
+                        {nlnv_about_tabs.nlnv_about_staff_3.nlnv_about_staff_phone !== '' && <a className="about__tab-icon text-link" href={`tel:${nlnv_about_tabs.nlnv_about_staff_3.nlnv_about_staff_phone}`}><FontAwesomeIcon icon={faPhone} /></a>}
+                      </div>
+                    </div>
+                    <div className={`col-md-${12 / staffActiveCount}`}>
+                      <img className="about__tab-image" src={nlnv_about_tabs.nlnv_about_staff_4.nlnv_about_staff_photo.source_url} alt="" />
+                      <p className="nlnv__subheading divider--wrap text--gold margin-top-sm">{nlnv_about_tabs.nlnv_about_staff_4.nlnv_about_staff_title}</p>
+                      <div className="about__tab-introduction">
+                        <p>{nlnv_about_tabs.nlnv_about_staff_4.nlnv_about_staff_name}</p>
+                        <a className="about__tab-icon text-link" href={`mailto:${nlnv_about_tabs.nlnv_about_staff_4.nlnv_about_staff_email}`}><FontAwesomeIcon icon={faEnvelope} /></a>
+                        {nlnv_about_tabs.nlnv_about_staff_4.nlnv_about_staff_phone !== '' && <a className="about__tab-icon text-link" href={`tel:${nlnv_about_tabs.nlnv_about_staff_4.nlnv_about_staff_phone}`}><FontAwesomeIcon icon={faPhone} /></a>}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="about__card-container">
+          <div className="row">
+            <div className="col-md-4 col-sm-6 about__card about__card--leader" style={{ backgroundImage: `url(${nlnv_about_service_grid.nlnv_about_service_grid_1.nlnv_about_service_image.source_url})` }}>
+              <h3 className="nlnv__heading--sm kor-main">{nlnv_about_service_grid.nlnv_about_service_grid_1.nlnv_about_service_title}</h3>
+              <hr className="divider divider--blue" />
+              <div className="about__card-info kor-main text-center" dangerouslySetInnerHTML={{ __html: nlnv_about_service_grid.nlnv_about_service_grid_1.nlnv_about_service_details }} />
+            </div>
+            <div className="col-md-4 col-sm-6 about__card about__card--faith" style={{ backgroundImage: `url(${nlnv_about_service_grid.nlnv_about_service_grid_2.nlnv_about_service_image.source_url})` }}>
+              <h3 className="nlnv__heading--sm kor-main">{nlnv_about_service_grid.nlnv_about_service_grid_2.nlnv_about_service_title}</h3>
+              <hr className="divider divider--blue" />
+              <div className="about__card-info kor-main text-center" dangerouslySetInnerHTML={{ __html: nlnv_about_service_grid.nlnv_about_service_grid_2.nlnv_about_service_details }} />
+            </div>
+            <div className="col-md-4 col-sm-6 about__card about__card--disciple" style={{ backgroundImage: `url(${nlnv_about_service_grid.nlnv_about_service_grid_3.nlnv_about_service_image.source_url})` }}>
+              <h3 className="nlnv__heading--sm kor-main">{nlnv_about_service_grid.nlnv_about_service_grid_3.nlnv_about_service_title}</h3>
+              <hr className="divider divider--blue" />
+              <div className="about__card-info kor-main text-center" dangerouslySetInnerHTML={{ __html: nlnv_about_service_grid.nlnv_about_service_grid_3.nlnv_about_service_details }} />
+            </div>
+            <div className="col-md-4 col-sm-6 about__card about__card--bible" style={{ backgroundImage: `url(${nlnv_about_service_grid.nlnv_about_service_grid_4.nlnv_about_service_image.source_url})` }}>
+              <h3 className="nlnv__heading--sm kor-main">{nlnv_about_service_grid.nlnv_about_service_grid_4.nlnv_about_service_title}</h3>
+              <hr className="divider divider--blue" />
+              <div className="about__card-info kor-main text-center" dangerouslySetInnerHTML={{ __html: nlnv_about_service_grid.nlnv_about_service_grid_4.nlnv_about_service_details }} />
+            </div>
+            <div className="col-md-4 col-sm-6 about__card about__card--pray" style={{ backgroundImage: `url(${nlnv_about_service_grid.nlnv_about_service_grid_5.nlnv_about_service_image.source_url})` }}>
+              <h3 className="nlnv__heading--sm kor-main">{nlnv_about_service_grid.nlnv_about_service_grid_5.nlnv_about_service_title}</h3>
+              <hr className="divider divider--blue" />
+              <div className="about__card-info kor-main text-center" dangerouslySetInnerHTML={{ __html: nlnv_about_service_grid.nlnv_about_service_grid_5.nlnv_about_service_details }} />
+            </div>
+            <div className="col-md-4 col-sm-6 about__card about__card--school" style={{ backgroundImage: `url(${nlnv_about_service_grid.nlnv_about_service_grid_6.nlnv_about_service_image.source_url})` }}>
+              <h3 className="nlnv__heading--sm kor-main">{nlnv_about_service_grid.nlnv_about_service_grid_6.nlnv_about_service_title}</h3>
+              <hr className="divider divider--blue" />
+              <div className="about__card-info kor-main text-center" dangerouslySetInnerHTML={{ __html: nlnv_about_service_grid.nlnv_about_service_grid_6.nlnv_about_service_details }} />
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  )
+      </>
+    );
+  }
 }
 
 AboutPage.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
+  acf: PropTypes.object
 }
 
 const Page = ({ data }) => {
@@ -36,7 +175,7 @@ const Page = ({ data }) => {
 
   return (
     <Layout>
-      <AboutPage title={page.title} content={page.content} acf={page.acf} />
+      <AboutPage acf={page.acf} />
     </Layout>
   )
 }
@@ -56,6 +195,106 @@ export const pageQuery = graphql`
         nlnv_page_title
         nlnv_hero_image {
           source_url
+        }
+        nlnv_about_tabs {
+          nlnv_about_tab_1_title
+          nlnv_about_main_vision
+          nlnv_about_vision_1
+          nlnv_about_vision_2
+          nlnv_about_vision_3
+          nlnv_about_tab_2_title
+          nlnv_about_introduction_title
+          nlnv_about_introduction
+          nlnv_about_introduction_photo {
+            source_url
+          }
+          nlnv_about_resume_title
+          nlnv_about_resume
+          nlnv_about_tab_3_title
+          nlnv_about_staff_1 {
+            nlnv_staff_active
+            nlnv_about_staff_title
+            nlnv_about_staff_name
+            nlnv_about_staff_email
+            nlnv_about_staff_phone
+            nlnv_about_staff_photo {
+              source_url
+            }
+          }
+          nlnv_about_staff_2 {
+            nlnv_staff_active
+            nlnv_about_staff_title
+            nlnv_about_staff_name
+            nlnv_about_staff_email
+            nlnv_about_staff_phone
+            nlnv_about_staff_photo {
+              source_url
+            }
+          }
+          nlnv_about_staff_3 {
+            nlnv_staff_active
+            nlnv_about_staff_title
+            nlnv_about_staff_name
+            nlnv_about_staff_email
+            nlnv_about_staff_phone
+            nlnv_about_staff_photo {
+              source_url
+            }
+          }
+          nlnv_about_staff_4 {
+            nlnv_staff_active
+            nlnv_about_staff_title
+            nlnv_about_staff_name
+            nlnv_about_staff_email
+            nlnv_about_staff_phone
+            nlnv_about_staff_photo {
+              source_url
+            }
+          }
+        }
+        nlnv_about_service_grid {
+          nlnv_about_service_grid_1 {
+            nlnv_about_service_title
+            nlnv_about_service_details
+            nlnv_about_service_image {
+              source_url
+            }
+          }
+          nlnv_about_service_grid_2 {
+            nlnv_about_service_title
+            nlnv_about_service_details
+            nlnv_about_service_image {
+              source_url
+            }
+          }
+          nlnv_about_service_grid_3 {
+            nlnv_about_service_title
+            nlnv_about_service_details
+            nlnv_about_service_image {
+              source_url
+            }
+          }
+          nlnv_about_service_grid_4 {
+            nlnv_about_service_title
+            nlnv_about_service_details
+            nlnv_about_service_image {
+              source_url
+            }
+          }
+          nlnv_about_service_grid_5 {
+            nlnv_about_service_title
+            nlnv_about_service_details
+            nlnv_about_service_image {
+              source_url
+            }
+          }
+          nlnv_about_service_grid_6 {
+            nlnv_about_service_title
+            nlnv_about_service_details
+            nlnv_about_service_image {
+              source_url
+            }
+          }
         }
       }
     }
